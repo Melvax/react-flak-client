4306cc5f4a672520e08f61abdca78309f21c5fc7
Melvax <melvin.adjei@gmail.com>
2023-05-26 15:43:08 +0200

    it works

--- src/App.js
+++ src/App.js
@@ -1,25 +1,78 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React ,{useState, useEffect} from 'react'
+import Select from 'react-select';
 function App() {
+
+  
+
+  const [selectedOptions, setSelectedOptions] = useState();
+  const [occupancyNumber, updateOccupancyNumber] = React.useState(0);
+  const [queriedRoom, updateQueriedRoom] = useState();
+
+
+
+
+   useEffect(()=>{
+    console.log("fetching api")
+    const currentDate = new Date();
+    const formattedDate = currentDate.toISOString();
+
+    const requestOptions = {
+      method: 'POST',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({"sensor":"abc","ts":formattedDate,"in":5,"out":1})
+  }
+
+  Promise.all([
+    fetch('/api/webhook', requestOptions).then(response => response.json()),
+  ]).catch(err => console.error(err));
+   },[]) 
+
+   const optionList = [
+    { value: "abc", label: "abc" },
+    { value: "xyz", label: "xyz" },
+    { value: "squaresense", label: "squaresense" },
+
+  ]
+
+  function handleSelect(data) {
+    setSelectedOptions(data);
+  }
+
+  function onClickButton() {
+    updateQueriedRoom(selectedOptions.value)
+    fetch("/occupancy?sensor="+selectedOptions.label).then(
+      res => res.json()
+    ).then(
+      data =>updateOccupancyNumber(data.inside)      
+    )
+  }
+    
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='body'>
+      <h2>Choose your sensor</h2>
+      <div className="dropdown-container">
+        <Select
+              options={optionList}
+              placeholder="Select room"
+              value={selectedOptions}
+              onChange={handleSelect}
+              isSearchable={true}
+              
+            />
+      </div>
+      <button className='button' onClick={onClickButton}>Show occupancy</button>
+
+      <div className="result">
+        {
+          (occupancyNumber==="no room data"||queriedRoom === undefined)?(<p>{queriedRoom} no room data   </p>):(<p>
+        Sensor {queriedRoom} reports from room occupancy of {occupancyNumber} people</p>)
+        }
+
+      </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
